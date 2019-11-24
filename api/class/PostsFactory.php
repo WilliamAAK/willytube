2cf47eff54a75606bfc004644385d297a6dfcfda
wilyahm <williamaak@gmail.com>
2019-11-24 22:41:53 +0100

    a

--- api/class/PostsFactory.php
+++ api/class/PostsFactory.php
@@ -8,7 +8,7 @@ class PostsFactory
         $db = Database::connect();
 
         # Query database and fetch results to array
-        $results = $db->query("SELECT uid, title, date FROM videos ORDER BY id desc");
+        $results = $db->query("SELECT uid, title, date FROM videos ORDER BY id asc");
         while ($dbs_row = $results->fetchArray(SQLITE3_ASSOC)) {
             $assoc_array[] = $dbs_row;
         }
